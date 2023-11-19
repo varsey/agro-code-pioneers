a85f85c87b3339c48ed1eab0ca0545a614cc52bc
varsey <eugeny.varseev@gmail.com>
2023-11-19 09:29:02 +0300

    refactor - files renaming
    - graphs now saved to 'graph' folder

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
+
 import pandas as pd
-from lib.plotting_utils import build_map
-from lib.Prosessor import Processor
+
+from lib.graph_utils import build_map
+from lib.processor import Processor
 
 p = Processor('data.csv')
 p.process()
+
 data = pd.read_csv('data_selected.csv')
 data_sample = data.sample(700)
+
 print(len(data_sample.jobs_selected.unique()))
-build_map(data_sample)
+results_folder = os.getcwd() + '/graph'
+build_map(data=data_sample, rfolder=results_folder)
